1c733d8354b7e5d54bc8dd4e3573070d69c920a7
Emily V <emily.venturino@outlook.com>
2021-11-09 18:48:22 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List.js";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="morningList">
+      <h1>Good Morning!</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-list-input" className="label__lg">
+            What am I grateful for today?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-list-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> Items</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> Items</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Past</span>
+          <span className="visually-hidden"> Items</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 items total
+      </h2>
+      <ul
+        role="list"
+        className="list-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        <List name="Cat"/>
+        <List name="Fuzzy Socks"/>
+        <List name="Coffee" />
+      </ul>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function List(props) {
+    return (
+        <li className="list stack-small">
+        <div className="c-cb">
+          <label className="list-label" htmlFor="list-1">
+            {props.name}
+          </label>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn">
+            Edit <span className="visually-hidden"> {props.name}</span>
+          </button>
+          <button type="button" className="btn btn__danger">
+            Delete <span className="visually-hidden"> {props.name}</span>
+          </button>
+        </div>
+      </li>
+    );
+  }
